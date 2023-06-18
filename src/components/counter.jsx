8153d5e172d6e9f5532723aed3183e84f46b3161
emamyari <emamyari@yahoo.com>
2023-06-18 17:56:25 +0430

    handleDel Function developed

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {count:this.props.tedad}
+    state = {}
+    handleDel=()=>{
+        alert(123)
+    }
     render() {
          let a=''
-         this.state.count==0?a='zero':a=this.state.count
+         this.props.tedad==0?a='zero':a=this.props.tedad
          return (
             <div>
                 <button className='btn btn-success m-1'>+</button>
                 <span className='bg-primary m-1 btn text-light'>{a} </span>
                 <button className='btn btn-warning m-1'>-</button>
-                <button className='btn btn-danger m-1'>delete</button>
+                <button onClick={this.handleDel} className='btn btn-danger m-1'>delete</button>
+                <span className='bg-primary m-1 btn text-light'>{this.props.title}</span>
             </div>
         );
     }
