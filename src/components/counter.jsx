92cf8a72a765b03c2a80f8d6619f0376922042be
emamyari <emamyari@yahoo.com>
2023-06-24 18:30:41 +0430

    add inc and dec functions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,7 +8,7 @@ class Counter extends Component {
          this.props.tedad==0?a='zero':a=this.props.tedad
          return (
             <div>
-                <button className='btn btn-success m-1'>+</button>
+                <button onClick={()=>this.props.hinc(this.props.id)} className='btn btn-success m-1'>+</button>
                 <span className='bg-primary m-1 btn text-light'>{a} </span>
                 <button className='btn btn-warning m-1'>-</button>
                 <button onClick={()=>this.props.f(this.props.id)} className='btn btn-danger m-1'>delete</button>
