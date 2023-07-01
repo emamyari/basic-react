4c53d5ca13a3a1443d0d84dd6bf428c473531245
emamyari <emamyari@yahoo.com>
2023-07-01 16:24:36 +0330

    add Nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Counter from './components/counter';
 import Counters from './components/counters';
+import Nav from './components/nav';
+ 
 
 class App extends Component {
   state = {}
   render() {
     return (
       <div>
+        <Nav></Nav>
        <Counters></Counters>
       </div>
     );
